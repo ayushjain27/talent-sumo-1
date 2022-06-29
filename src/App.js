cf31b755421e3505f7f527c67f2d7940d5e73128
ayushjain27 <ayushjain201574@gmail.com>
2022-06-29 21:53:14 +0530

    Added Table form

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Route path='/' element={<Create />} />
           <Route path='/access' element={<Access />} />
         </Routes>
+        
       </Router>
     </>
   );
